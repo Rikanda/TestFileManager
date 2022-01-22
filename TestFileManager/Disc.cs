a757c98a32bee4f024371fe6b4fef36ad1510ee2
Natalya <soltk7@yandex.ru>
2022-01-22 23:36:29 +0300

    Add project files.

---
+++ TestFileManager/Disc.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace TestFileManager
+{
+    public class Disc
+    {
+        
+        public string name { get; set; }
+        public int index { get; set; }
+
+        public Disc(string name_, int index_)
+        {
+            
+            this.name = name_;
+            this.index = index_;
+        }
+
+        public void DiscIntro()
+        {
+            string[] intro = System.IO.Directory.GetDirectories(name);
+            foreach(string str in intro)
+            {
+                Console.WriteLine(str);
+            }
+
+        }
+    }
+
+    public class DiscList
+    {
+        public List<Disc> list { get; set; }
+
+        public DiscList()
+        {
+            
+            DriveInfo[] drv = DriveInfo.GetDrives();
+
+            this.list = new List<Disc>();
+            string i;
+            int n = 0;
+            foreach (DriveInfo drv_ in drv)
+            {
+                i = drv_.Name;
+                Disc d = new Disc(i,n);
+                list.Add(d);
+                n++;
+            }
+ 
+        }
+
+        public void prntDiscList()
+        {
+            foreach (Disc d in list)
+            {
+                Console.WriteLine(d.name);
+            }
+        }
+
+        public void prntDirectory(int index)
+        {
+            foreach(Disc d in list)
+            {
+                if (d.index == index)
+                {
+                    d.DiscIntro();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TestFileManager/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestFileManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscList l1 = new DiscList();
+            int n = l1.list.Count;
+            Console.WriteLine(n);
+            l1.prntDiscList();
+            Console.WriteLine("Укажите порядковый номер диска для раскрытия списка каталогов:");
+            int t = int.Parse(Console.ReadLine());
+            l1.prntDirectory(t-1);
+
+        }
+    }
+}
